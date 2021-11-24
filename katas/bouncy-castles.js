b1b3fc1af2f083156425760f8dcaa5c10fc83652
leepavelich <leepavelich@MacBook-Pro.local>
2021-11-24 13:40:31 -0700

    Refactored bouncy-castles.js to use arrow function notation

--- katas/bouncy-castles.js
+++ katas/bouncy-castles.js
@@ -1,9 +1,7 @@
 // Use the value below whenever you need the value of Pi
 const PI = 3.14159 ;
 
-const sphereVolume = function (radius) {
-  return (4/3) * PI * radius ** 3
-}
+const sphereVolume = radius => (4/3) * PI * radius ** 3
 
 console.log(4186 < sphereVolume(10) && sphereVolume(10) < 4189);
 
@@ -13,13 +11,11 @@ const coneVolume = function (radius, height) {
 
 console.log(45 < coneVolume(3, 5) && coneVolume(3, 5) < 49);
 
-const prismVolume = function (height, width, depth) {
-  return height * width * depth
-}
+const prismVolume = (height, width, depth) => height * width * depth
 
 console.log(prismVolume(3, 4, 5) === 60);
 
-const totalVolume = function (solids) {
+const totalVolume = solids => {
   let volume = 0
 
   for (let solid of solids) {
@@ -33,7 +29,6 @@ const totalVolume = function (solids) {
       volume += prismVolume(solid.height, solid.width, solid.depth)
     }
   }
-
   return volume
 }
 
