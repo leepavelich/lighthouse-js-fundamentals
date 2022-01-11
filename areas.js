fe4523b1f6ffd61374d0786229ac0955ca1a227b
Lee Pavelich <leepavelich@gmail.com>
2022-01-11 19:15:03 +0000

    Updated areas

--- areas.js
+++ areas.js
@@ -1,25 +1,12 @@
-function calculateRectangleArea(length, width) {
-  if (length < 0 || width < 0) {
-    return undefined
-  } else {
-    return length * width
-  }
-}
+const calculateRectangleArea = (l, w) => {
+  if (length > 0 && width > 0) return l * w;
+};
+
+const calculateTriangleArea = (b, h) => {
+  if (base > 0 && height > 0) return b * h / 2;
+};
 
 
-function calculateTriangleArea(base, height) {
-  if (base < 0 || height < 0) {
-    return undefined
-  } else {
-    return base * height / 2
-  }
-}
-
-
-function calculateCircleArea(radius) {
-  if (radius < 0) {
-    return undefined
-  } else {
-    return Math.PI * radius * radius
-  }
+function calculateCircleArea(r) {
+  if (radius > 0) return Math.PI * r * r;
 }
